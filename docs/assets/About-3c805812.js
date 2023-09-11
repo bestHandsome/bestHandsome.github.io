c081eec7163d00dc591db48cc88ce078189ee279
jinjin <769537915@qq.com>
2023-09-11 09:30:55 +0800

    feat: 删除文件

--- docs/assets/About-3c805812.js
+++
@@ -1,8 +0,0 @@
-import { _ as e } from './_plugin-vue_export-helper-c27b6911.js';
-import { am as o, an as t } from './index-06154a72.js';
-const r = {};
-function c(n, a) {
-	return o(), t('div', null, 'about -------');
-}
-const f = e(r, [['render', c]]);
-export { f as default };
--- docs/assets/Board-ba867e37.js
+++
@@ -1,8 +0,0 @@
-import { _ as r } from './_plugin-vue_export-helper-c27b6911.js';
-import { am as e, an as o } from './index-06154a72.js';
-const a = {};
-function c(n, t) {
-	return e(), o('div', null, 'board------');
-}
-const f = r(a, [['render', c]]);
-export { f as default };
